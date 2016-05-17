1715799cc35951dba213f6a6570807e3c9a056cd
Vocho <vocho@underdog.gs>
2016-05-17 15:51:35 -0500

    443tqtr
    rett

--- js/script.js
+++ js/script.js
@@ -4,9 +4,43 @@ $(document).ready(function(){
 		$("#portada").height(screenHeight());
 	});
 	$("article").concentradora();
+
+	$("#nombres-estados > div").each(function(){
+		$(this).hover(inEstados, outEstados);
+		$(this).focus(inEstados);
+		$(this).blur(outEstados);
+	});
+	$(".mapa-estados").each(function(){
+		$(this).hover(inEstados, outEstados);
+		$(this).focus(inEstados);
+		$(this).blur(outEstados);
+	});
 });
 
-
+function inEstados(){
+	var estado = $(this).attr('class').split(" ")[1];
+	var estado_class = "."+estado;
+	$(estado_class).each(function(){
+		if(this.tagName == 'polygon'){
+			$(this).attr('class', 'mapa-estados '+estado+' active');
+		}else{
+			$(this).addClass("active");
+		}
+	});
+	//$(estado).addClass("active");
+}
+function outEstados(){
+	var estado = $(this).attr('class').split(" ")[1];
+	var estado_class = "."+estado;
+	$(estado_class).each(function(){
+		if(this.tagName == 'polygon'){
+			$(this).attr('class', 'mapa-estados '+estado);
+		}else{
+			$(this).removeClass("active");
+		}
+	});
+	
+}
 
 function barsDisapear(){
 	$("#nav-bar-stats").remove();
